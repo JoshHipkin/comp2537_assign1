adb60c8c693ef13054acbd5b98dd504052df1f00
Josh Hipkin <j.hipkin4@gmail.com>
2023-04-28 19:35:43 -0700

    security fixes

--- databaseConnection.js
+++ databaseConnection.js
@@ -5,7 +5,7 @@ const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 
 const MongoClient = require("mongodb").MongoClient;
-const atlasURI = `mongodb+srv://jhipkin:HuoivE9OpybvMODW@cluster0.j5iks8k.mongodb.net/?retryWrites=true&w=majority`;
+const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}.j5iks8k.mongodb.net/?retryWrites=true&w=majority`;
 var database = new MongoClient(atlasURI, {useNewUrlParser: true, useUnifiedTopology: true});
 database.connect((err) => {
     if (err) {
