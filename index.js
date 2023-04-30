d5b474e43835310fbe0547d45dfecc6fabe807a6
Josh Hipkin <j.hipkin4@gmail.com>
2023-04-29 22:48:29 -0700

    mongodb connection

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
 
 const session = require('express-session');
 const express = require('express');
@@ -12,27 +17,22 @@ require("./utils.js");
 const app = express();
 const port = process.env.PORT || 8000;
 
- 
-const mongodb_host = process.env.MONGODB_HOST;
-const mongodb_user = process.env.MONGODB_USER;
-const mongodb_password = process.env.MONGODB_PASSWORD;
-const mongodb_database = process.env.MONGODB_DATABASE;
-const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
-const node_session_secret = process.env.NODE_SESSION_SECRET;
-
-var {database} = include('databaseConnection');
-
-const userCollection = database.db(mongodb_database).collection('users');
+ var users = [];
 
 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+const { database } = include("databaseConnection");
+
+const userCollection = database.db(mongodb_database).collection("users");
+
+
+
+app.use(express.urlencoded({ extended: false }));
 
 var mongoStore = MongoStore.create({
-    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@Cluster0/sessions`,
     crypto: {
         secret: mongodb_session_secret
-    }
+    },
 });
 
 app.use(session({
